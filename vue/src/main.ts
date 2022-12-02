32de33bc5fa3f8ba17451b9acb27af0c09c878e0
9t5c <uucc@live.com>
2022-12-02 11:39:12 +0800

    vuetify treeshaking

--- vue/src/main.ts
+++ vue/src/main.ts
@@ -6,16 +6,22 @@ import router from "./router";
 
 import "vuetify/styles";
 import { createVuetify } from "vuetify";
-import * as components from "vuetify/components";
-import * as directives from "vuetify/directives";
+import { Ripple } from "vuetify/directives";
+import { VBtn } from "vuetify/components/VBtn";
+import { VIcon } from "vuetify/components/VIcon";
+import { VList, VListItem } from "vuetify/components/VList";
+import { VMenu } from "vuetify/components/VMenu";
+import { VProgressLinear } from "vuetify/components/VProgressLinear";
 
 import "@/assets/base.css";
 
 const app = createApp(App);
 
 const vuetify = createVuetify({
-  components,
-  directives,
+  components: { VBtn, VIcon, VList, VListItem, VMenu, VProgressLinear },
+  directives: {
+    Ripple,
+  },
 });
 
 app.use(createPinia());
